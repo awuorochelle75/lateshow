9c4629dedc57ab90fe6228d6fa634029edcf0f4f
Rochelle Awuor <awuorochelle75@gmail.com>
2025-06-23 14:28:42 +0300

    modifies files

--- app/seed.py
+++ app/seed.py
@@ -9,14 +9,14 @@ with app.app_context():
     db.create_all()
 
     # Create Guests
-    guest1 = Guest(name="Trevor Noah")
-    guest2 = Guest(name="Lupita Nyong'o")
-    guest3 = Guest(name="Ali Kiba")
+    guest1 = Guest(name="Trevor Noah", occupation="Comedian")
+    guest2 = Guest(name="Lupita Nyong'o", occupation="Actress")
+    guest3 = Guest(name="Ali Kiba", occupation="Musician")
 
-    # Create Episodes
-    episode1 = Episode(title="Late Show with Comedy Legends")
-    episode2 = Episode(title="Afro Stars Special")
-    episode3 = Episode(title="Hollywood Takeover")
+    # Create Episodes (using date and number, not title!)
+    episode1 = Episode(date="1/11/99", number=1)
+    episode2 = Episode(date="1/12/99", number=2)
+    episode3 = Episode(date="1/13/99", number=3)
 
     # Add to session so we can access their IDs
     db.session.add_all([guest1, guest2, guest3, episode1, episode2, episode3])
